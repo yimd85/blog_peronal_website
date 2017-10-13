9dd5ea5f7c3cdf43eb8bfe606a52c40091eb89e2
David Yim <yimd85@gmail.com>
2017-10-13 17:56:54 -0400

    using models

--- appExpress.js
+++ appExpress.js
@@ -1,39 +1,60 @@
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://yimd85@localhost:5432/bulletinboard');
 
 var express = require('express')
 var path    = require("path");
 var bodyParser= require('body-parser')
+var Stuff = require("./models/models.js");
 var app = express();
-
-
+//
+// const { Client } = require('pg');
+//
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
+//
+// client.connect();
+//
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 
 app.set('view engine','ejs');
-app.set('views','./views')
-app.use(express.static(path.join(__dirname,'images')))
-app.use(express.static(path.join(__dirname,'css')))
-app.use(express.static(path.join(__dirname,'SoundCloudAPIFinal')))
-app.use(express.static(path.join(__dirname,'mySnake')))
+// app.set('views','./views')//dont need this
+app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname,'images')))//dont need this
+// app.use(express.static(path.join(__dirname,'css')))//dont need this
+// app.use(express.static(path.join(__dirname,'SoundCloudAPIFinal')))//dont need this
+// app.use(express.static(path.join(__dirname,'mySnake')))//dont need this
 app.use(bodyParser.urlencoded({ extended: true })) //needed for comments/posts
 
 
+// var Sequelize = require('sequelize');
+// var sequelize = new Sequelize('postgres://yimd85@localhost:5432/bulletinboard');
 
 
-//databasestuff
-var Stuff = sequelize.define('boardpost', {
-  user: Sequelize.STRING(100),
-  post: Sequelize.TEXT
-});
+// var Stuff = sequelize.define('boardpost', {
+//   user: Sequelize.STRING(100),
+//   post: Sequelize.TEXT
+// });
+//
+//
+// Stuff//creating first entry
+// .sync()
+// .then(function(){
+//   Stuff.create({
+//     user: 'MyspaceTom',
+//     post: 'hello friend!'
+//   });
+// });
+//
 
-Stuff//creating first entry
-.sync()
-.then(function(){
-  Stuff.create({
-    user: 'MyspaceTom',
-    post: 'hello friend!'
-  });
-});
+
+
 
 app.get('/gotoblog',function(request,response){
   Stuff.
@@ -42,23 +63,6 @@ app.get('/gotoblog',function(request,response){
           response.render('blog',{messages:rows});
         });
 });
-
-app.get('/addshit',function(request,response){
-  response.render('addMessages')
-});
-
-app.post('/addshit',function(request,response){
-  Stuff
-    .findAll()
-      .then(function(){
-        Stuff.create({
-            user: request.body.user,
-            post: request.body.post
-        });
-        response.redirect('/gotoblog')
-  });
-});
-
 
 app.post('/gotoblog',function(request,response){
   Stuff
---
+++ models/models.js
@@ -0,0 +1,38 @@
+var Sequelize = require('sequelize');
+// var sequelize = new Sequelize('postgres://yimd85@localhost:5432/bulletinboard');
+
+
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  var sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  });
+}
+
+
+
+//databasestuff
+var Stuff = sequelize.define('boardpost', {
+  user: Sequelize.STRING(100),
+  post: Sequelize.TEXT
+});
+
+
+Stuff//creating first entry
+.sync()
+.then(function(){
+  Stuff.create({
+    user: 'MyspaceTom',
+    post: 'hello friend!'
+  });
+});
+
+Stuff.sync();
+
+module.exports = Stuff;
